99dc011e16ff89b7b6a03b636563d35fadfcc11c
Riane <riane.dossantos75@gmail.com>
2022-11-16 18:43:02 -0300

    Added encrypt SHA256 util

---
+++ CarBom/Utils/EncryptUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CarBom.Utils
+{
+    public static class EncryptUtil
+    {
+        public static string EncryptToSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                //ComputeHash - generate byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                //Convert byte array to string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
